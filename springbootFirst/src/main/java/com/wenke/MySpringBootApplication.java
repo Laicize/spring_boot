f637de630cffd5164eda774a39e7c28c5e39f37b
Laicize <wangwenzhong12@gmail.com>
2019-09-05 14:54:09 +0800

    练习springBoot的库

---
+++ springbootFirst/src/main/java/com/wenke/MySpringBootApplication.java
@@ -0,0 +1,15 @@
+package com.wenke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 王文重
+ */
+@SpringBootApplication
+public class MySpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootApplication.class);
+    }
+}
---
+++ springboot_second/src/main/java/com/wenke/springboot_second/SpringbootSecondApplication.java
@@ -0,0 +1,13 @@
+package com.wenke.springboot_second;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSecondApplication.class, args);
+    }
+
+}
